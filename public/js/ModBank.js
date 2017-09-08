7c5ce91955c0afe78f8e0ad3de0d8367efb099d2
Wasif Zaman <teatocode@gmail.com>
2017-09-08 09:08:11 -0400

    combined to one file

--- public/js/ModBank.js
+++ public/js/ModBank.js
@@ -23,3 +23,22 @@ class Store {
     })
   }
 }
+
+class Mod {
+  constructor(id, store, ...bankNames) {
+    this.id = id
+    this.store = store
+    bankNames.forEach((bankName)=> {
+      this.store.addModule(bankName, this)
+    })
+  }
+  render() {
+    this.$html = $(``)
+    $(this.id).empty()
+    $(this.id).append(this.$html)
+  }
+  update(newData) {
+    this.store = newData
+    this.render()
+  }
+}
